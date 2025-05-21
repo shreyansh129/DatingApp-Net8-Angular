82accc5b5bbc1cf5f80b82d3feeb58a7fcead2db
Shreyansh Shah <shahshreyansh2000@gmail.com>
2025-05-21 17:17:59 +0530

    End of Section 8

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_model/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [],
+  imports: [TabsModule, NgImageSliderModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
+  galleryImages: { image: string, thumbImage: string }[] = [];
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+        member.photos.map(p => {
+          this.galleryImages = member.photos.map(p => ({
+            image: p.url,
+            thumbImage: p.url
+          }))
+        })
+      }
+    })
+  }
 }
